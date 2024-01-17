421bad286cde83ab1f83f3b1cd88be8bd7dc788f
NieBoczek <bartekkoraliki@gmail.com>
2024-01-17 16:42:04 +0100

    first git commit

---
+++ plz.py
@@ -0,0 +1,152 @@
+import sys
+import theliblib as tl
+from theliblib import SubCmd, ArgType
+import random
+import os
+import shutil
+import requests
+import toml
+from bs4 import BeautifulSoup
+
+
+def helpfunc(topic: str = None):
+    if topic == 'run':
+        return "Usage: plz run <alias>\nRuns the file binded to the alias."
+    elif topic == 'random':
+        return "Usage: plz random\nSelects a random game and runs it."
+    elif topic == 'fetch':
+        return "Usage: plz fetch <game>\nFetches download links for that game."
+    elif topic == 'edit':
+        return "Usage: plz edit\nOpens VSCode where this CLI tool is."
+    elif topic == 'alias list':
+        return "Usage: plz alias list\nPrints a list of all aliases."
+    elif topic == 'alias add':
+        return "Usage: plz alias add <name> <point>\nAdds an alias to the list."
+    elif topic == 'alias remove':
+        return "Usage: plz alias remove <name>\nRemoves an alias from the list."
+    return \
+"""
+Usage: plz help - Shows this menu.
+       plz run <alias> - Runs the file binded to the alias.
+       plz random - Selects a random game and runs it.
+       plz fetch <game> - Fetches download links for that game.
+       plz edit - Opens VSCode where this CLI tool is.
+       plz alias list - Prints a list of all aliases.
+       plz alias add <name> <point> - Adds an alias to the list.
+       plz alias remove <name> - Removes an alias from the list.
+"""
+
+
+try:
+    with open(tl.get_dir() + 'config.toml') as f:
+        cfg = toml.load(f)
+    with open(tl.get_dir() + 'aliases.toml') as f:
+       aliases = toml.load(f)
+except FileNotFoundError:
+    with open(tl.get_dir() + 'config.toml', 'w') as f:
+        toml.dump({'runin': '', 'clear_runin': False}, f)
+    with open(tl.get_dir() + 'aliases.toml', 'w') as f:
+        toml.dump({}, f)
+    cfg = {'runin': None, 'clear_runin': False}
+    aliases = {}
+
+
+def save_config():
+    with open(tl.get_dir() + 'config.toml', 'w') as f:
+        toml.dump(cfg, f)
+
+
+def save_aliases():
+    with open(tl.get_dir() + 'aliases.toml', 'w') as f:
+        toml.dump(aliases, f)
+
+
+def run(alias: str):
+    try:
+        os.system(f'"{aliases[alias]}"')
+        if cfg['clear_runin']:
+            try: shutil.rmtree(cfg['runin'])
+            except PermissionError: pass
+    except KeyError:
+        return f'No alias found named "{alias}"'
+
+
+def sub_random():
+    key = random.choice([k for k in aliases.keys()])
+    run(key)
+
+
+def sub_edit():
+    os.system('code ' + tl.get_dir())
+
+
+def sub_alias_list():
+    max_len = max(len(game) for game in aliases.keys()) + 1
+    for alias, path in aliases.items():
+        print(f'{alias.ljust(max_len)}{path}')
+
+
+def sub_alias_add(alias: str, point: str):
+    aliases.update({alias: point})
+    save_aliases()
+
+
+def sub_alias_remove(alias: str):
+    aliases.remove(alias)
+    save_aliases()
+
+
+def sub_fetch(name: str):
+    res = requests.get("https://steamrip.com/" + name)
+    soup = BeautifulSoup(res.text, "html.parser")
+    items = soup.find_all("p", {"style": "text-align: center;"})
+    if not items:
+        return "Game not found"
+    items.pop(0)
+    try:  # Try old game compatibility
+        info_list = soup.find('div', {"class": "plus tie-list-shortcode"}).find('ul').find('li').find('ul').find_all('li')
+    except AttributeError:
+        info_list = soup.find('div', {"class": "plus tie-list-shortcode"}).find('ul').find_all('li')
+    
+    size = info_list[3].get_text()
+    ver: str = info_list[5].get_text()
+    t = ver.partition(' | ')
+    print(t[0].encode('ascii', 'ignore').decode())  # Avoid getting UTF-8 Error in runner
+    print(size)
+    print("Download links:")
+    for elem in items:
+        print(f"- {elem.find("span").text.rstrip()}: https:{elem.find('a').get('href')}")
+
+
+def sub_update():
+    ...
+
+
+# def sub_config(attribute_name: str, value: str | bool):
+#     cfg.update({attribute_name: value})
+#     save_config()
+
+
+def check_for_updates():
+    ...
+
+
+def main():
+    cli = tl.CLITool()
+    cli.add_subcmd(SubCmd("random", sub_random))
+    cli.add_subcmd(SubCmd("edit", sub_edit))
+    cli.add_subcmd(SubCmd("run", run, [ArgType(str, options=aliases.keys())]))
+    cli.add_subcmd(SubCmd("fetch", sub_fetch, [ArgType(str)]))
+    cli.add_subcmd(SubCmd("update", sub_update))
+    # cli.add_subcmd(SubCmd("config", sub_config, [ArgType(str, options=['runin', 'clear_runin']), ArgType(str | bool)]))
+    cli.add_subcmd(SubCmd("alias", subcmds=[
+        SubCmd("list", sub_alias_list),
+        SubCmd("add", sub_alias_add, [ArgType(str), ArgType(str)]),
+        SubCmd("remove", sub_alias_remove, [ArgType(str, options=aliases.keys())])
+    ]))
+    os.chdir(runin) if (runin := cfg['runin']) else ''
+    cli.run(sys.argv, helpfunc, check_for_updates)
+
+
+if __name__ == '__main__':
+    main()
---
+++ theliblib.py
@@ -0,0 +1,201 @@
+import __main__
+from platform import system as platform
+# TODO: Handle union types in ArgType.test
+
+
+def to_seconds(human_input: str) -> int:
+    """
+    Converts a human input into seconds
+    Example: 1y 2mo 3d 4h 5m 6s
+    """
+    time = str(human_input).split()
+    years, months, days, hours, minutes, seconds = 0, 0, 0, 0, 0, 0
+    for t in time:
+        if 'y' in t:
+            years = int(t[:-1])
+        elif 'mo' in t:
+            months = int(t[:-2])
+        elif 'd' in t:
+            days = int(t[:-1])
+        elif 'h' in t:
+            hours = int(t[:-1])
+        elif 'm' in t:
+            minutes = int(t[:-1])
+        elif 's' in t:
+            seconds = int(t[:-1])
+        seconds = seconds + (minutes * 60) + (hours * 3600) + (days * 86400) + (months * 2419200) + (years * 29030400)
+    return seconds
+
+
+def get_dir():
+    """
+    Get path to folder where your script is located
+    """
+    if platform() == "Windows":
+        fp = __main__.__file__.split('\\')
+        fp.pop()
+        return str(fp).replace("'", "").replace(', ', '\\').replace('[', '').replace(']', '') + "\\"
+    elif platform() == "Linux":
+        fp = __main__.__file__.split('/')
+        fp.pop()
+        return str(fp).replace("'", "").replace(', ', '/').replace('[', '').replace(']', '') + "/"
+    else:
+        raise NotImplementedError("Function is not supported on " + platform())
+
+
+def cinput(prompt: str, requested_type: type = None, default=''):
+    """
+    Asks user with the `prompt`, and if `requested_type` is not none, tries to convert the user's
+    input to the `requested_type`\n
+    `requested_type` - requested type of output (int, str, etc.)
+    """
+    while True:
+        try:
+            user_input = input(prompt)
+            if user_input == '':
+                return default
+            if not requested_type is None:
+                user_input = requested_type(user_input)
+            break
+        except ValueError:
+            return None
+    return user_input
+
+
+def mass_replace(s: str, to_replace: list[str], replace_to: str):
+    for r in to_replace:
+        s = s.replace(r, replace_to)
+    return s
+
+
+class ArgType:
+    def __init__(self, _type: type, required: bool = True, options: list = None, continuos: bool = False) -> None:
+        self.type = _type
+        self.required = required
+        self.continuos = continuos
+        self.options = options or []
+
+    def test(self, input: str):
+        verdict = 'Wrong type'
+        try:
+            input = self.type(input)
+            verdict = f'Wrong option (Options: {mass_replace(str(self.options), ['[', ']', "'", '"'], '')})'
+            if self.options:
+                if input not in self.options:
+                    raise TypeError
+        except TypeError:
+            return verdict
+        return 'pass'
+
+
+class SubCmd:
+    def __init__(self, name: str, func=None, arguments: list[ArgType] = None, subcmds: list = None) -> None:
+        self.id = name
+        self.func = func
+        self.subcmds = subcmds or []
+        self.arguments = arguments or []
+        self.path = None
+
+    def _gen_path(self, subcmd):
+        for sub in subcmd.subcmds:
+            sub.path = subcmd.path + ' ' + sub.id
+            if sub.subcmds:
+                self._gen_path(sub)
+
+    def run(self, args):
+        if self.func:
+            res = self.func(*args)
+            if res is not None:
+                print(res)
+                return res
+
+
+class CLITool:
+    def __init__(self) -> None:
+        self.tree = SubCmd("Tree")
+        self.add_tree = False
+
+    def add_subcmd(self, subcmd: SubCmd) -> None:
+        subcmd.path = subcmd.id
+        if subcmd.subcmds:
+            subcmd._gen_path(subcmd)
+        self.tree.subcmds.append(subcmd)
+
+    def into_subcmd(self) -> None:
+        self.add_tree = True
+
+    def add_arg(self, arg_type: ArgType) -> None:
+        if not self.add_tree:
+            raise AttributeError("CLITool instance hasn't been turned into a subcommand.")
+        self.tree.arguments.append(arg_type)
+
+    def _get_stack(self, args: list[str], helpfunc) -> list[SubCmd, str]:
+        if self.add_tree:
+            args.insert(0, self.tree)
+        stack = []
+        args_left = []
+        for arg in args:
+            if len(args_left) > 0:
+                if args_left[0].continuos:
+                    i = args.index(arg)
+                    z = mass_replace(str(args[i:]), ['[', ']', "'"], '')
+                    z = z.replace(', ', ' ')
+                    stack.append(z)
+                    args_left.pop(0)
+                    break
+                if not ((verdict := args_left[0].test(arg)) == 'pass'):
+                    if helpfunc:
+                        stack.reverse()
+                        for item in enumerate(stack):
+                            if isinstance(item, SubCmd):
+                                if helpfunc:
+                                    print(r) if (r := helpfunc(item.path)) else 0
+                    print(f"\n`{arg}`: {verdict}")
+                    exit(1)
+                args_left.pop(0)
+                stack.append(arg)
+            elif self._is_subcmd(arg):
+                for subcmd in self._get_all_subcmds(self.tree):
+                    if subcmd.id == arg:
+                        stack.append(subcmd)
+                        args_left = subcmd.arguments.copy()
+                        break
+            else:
+                if helpfunc:
+                    print(r) if (r := helpfunc()) else 0
+                exit(1)
+        if args_left:
+            stack.reverse()
+            for item in stack:
+                if isinstance(item, SubCmd):
+                    if helpfunc:
+                        print(r) if (r := helpfunc(item.path)) else 0
+            exit(1)
+        return stack
+
+    def _is_subcmd(self, string: str) -> bool:
+        for subcmd in self._get_all_subcmds(self.tree):
+            if subcmd.id == string:
+                return True
+        return False
+
+    def _get_all_subcmds(self, subcmd: SubCmd) -> list[SubCmd]:
+        subcmds = []
+        for _ in range(len(subcmd.subcmds)):
+            for sub in subcmd.subcmds:
+                subcmds.append(sub)
+                subcmds.extend(self._get_all_subcmds(sub))
+        return subcmds
+
+    def run(self, argv: list[str], helpfunc = None, always_run = None) -> None:
+        if helpfunc:
+            self.add_subcmd(SubCmd("help", helpfunc, [ArgType(str, continuos=True)]))
+        stack = self._get_stack(argv[1:], helpfunc)
+        stack.reverse()
+        for i, item in enumerate(stack):
+            if isinstance(item, SubCmd):
+                r = stack[:i]
+                r.reverse()
+                item.run(r)
+                if always_run:
+                    print(z) if (z := always_run()) else 0
